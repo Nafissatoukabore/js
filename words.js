9709af515692c1c31c431fb1053cbe5a2b0c4fb7
nafissatoukabore <nafissatou.kabore@uvci.edu.ci>
2017-12-09 15:27:17 +0000

    multiply words

--- words.js
+++ words.js
@@ -7,8 +7,12 @@
  * @next total
  */
 
-
 //* Begin of tests
+const words = string => {
+  const resultat = string.split(' ')
+
+  return resultat
+}
 const assert = require('assert')
 
 assert.strictEqual(typeof words, 'function')
@@ -16,5 +20,5 @@ assert.strictEqual(words.length, 1)
 assert.deepStrictEqual(words('a b c'), [ 'a', 'b', 'c' ])
 assert.deepStrictEqual(words('a 1 c'), [ 'a', '1', 'c' ])
 assert.deepStrictEqual(words('a 1 c d e f'), [ 'a', '1', 'c', 'd', 'e', 'f' ])
-assert.deepStrictEqual(words('wol.lol lo,lol'), [ 'wol.lol', 'lo,lol'])
+assert.deepStrictEqual(words('wol.lol lo,lol'), [ 'wol.lol', 'lo,lol' ])
 // End of tests */
